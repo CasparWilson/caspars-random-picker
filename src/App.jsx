e420e4400d648aea248c8190364e067238ce65f9
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-04 14:24:54 +0100

    basic functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+import React from 'react'
 
 function App() {
+
+    const [thingsToPick, setThingsToPick] = React.useState("")
+    const [randomPick, setRandomPick] = React.useState("Click Button above to Pick!")
+
+    function handleChange(event){
+        setThingsToPick(event.target.value)
+    }
+
+    function handleClick(){
+        const thingsArray = thingsToPick.split('\n')
+        setRandomPick(thingsArray[Math.floor(Math.random() * thingsArray.length)])
+    }
+
     return (
         <div>
-            <h1>App component</h1>
-            <DemonstratingProps />
+            <h1>Random Picker</h1>
+            <textarea placeholder="Write things to pick from on different lines in here!" value={thingsToPick}
+            onChange={handleChange}/>
+            <button onClick={handleClick}>Pick!</button>
+            <h2>{randomPick}</h2>
         </div>
+
     );
 }
 
